2653ea9c9884bdc14c16e826927fbe43cbb8f70a
Ricenquimac <117040339+Ricenquimac@users.noreply.github.com>
2024-11-21 22:12:38 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,8 @@
+print("Hello World")
+import pandas as pd
+data_frame = pd.read_csv(".learn/assets/pokemon_data.csv")
+
+print(data_frame)
+
+data = pd.Series([23,45,7,34,6,63,36,78,54,34])
+print(data)
